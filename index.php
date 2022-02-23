9628d299f3edbe6ff9d58cfa3a4a1fdc587a5eea
GoalTV@ <j.czeropski@outlook.com>
2022-02-23 16:40:34 +0000

    Clean Up Code

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <html>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/template/header_tpl.php'); //Calling Header ?>
 <body>
-    <div id="Content ">
+
         <?php //Calling Content from /template
+
             if(!isset($_GET['page']) || $_GET['page'] == ''){
                 $page = 'home'; //If no page specified
-            } else {
+            } 
+            else 
+            {
                 $page = $_GET['page'];
             }
 
-            switch($page)
-            {
-                case $page:
-                    include($_SERVER['DOCUMENT_ROOT'].'/template/' . $page . '_tpl.php'); //Get Php files from /template/
-                    break;
+            include($_SERVER['DOCUMENT_ROOT'].'/template/' . $page . '_tpl.php'); //Get Php files from /template
 
-            }
         ?>
-    </div>
+
     </body>
     <div id="footer">
         <?php include($_SERVER['DOCUMENT_ROOT'].'/template/footer_tpl.php'); //Calling Footer ?>
