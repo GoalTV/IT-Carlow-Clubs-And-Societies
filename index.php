91162e1e7920e73c718bca5e654ca8d988e798ae
GoalTV@ <j.czeropski@outlook.com>
2022-02-23 18:58:55 +0000

    Tweak Template Layout

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<html>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/template/header_tpl.php'); //Calling Header ?>
-<body>
+<?php
 
-        <?php //Calling Content from /template
+/**
+*     Index Page.
+*     Project for Institute of Technology Carlow
+*     @Janusz Czeropski C00176885@itcarlow.ie 
+*     2022
+*/
 
-            if(!isset($_GET['page']) || $_GET['page'] == ''){
-                $page = 'home'; //If no page specified
-            } 
-            else 
-            {
-                $page = $_GET['page'];
-            }
 
-            include($_SERVER['DOCUMENT_ROOT'].'/template/' . $page . '_tpl.php'); //Get Php files from /template
+//Render Website
+require_once  $_SERVER['DOCUMENT_ROOT'].'/template/template.php';
 
-        ?>
-
-    </body>
-    <div id="footer">
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/template/footer_tpl.php'); //Calling Footer ?>
-    </div>
-</html>
+?>
