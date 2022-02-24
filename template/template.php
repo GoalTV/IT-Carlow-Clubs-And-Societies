569369fec66467e13db6ed401a56eb0352310433
GoalTV@ <j.czeropski@outlook.com>
2022-02-24 22:53:41 +0000

    Registration Form

---
+++ template/template.php
@@ -0,0 +1,30 @@
+<!--
+*     Template Layout.
+*     Project for Institute of Technology Carlow
+*     @Janusz Czeropski C00176885@itcarlow.ie 
+*     2022
+-->
+
+<html>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/template/header_tpl.php'); //Calling Header ?>
+<body>
+
+        <?php //Calling Content from /template
+
+            if(!isset($_GET['page']) || $_GET['page'] == ''){
+                $page = 'home'; //If no page specified
+            } 
+            else 
+            {
+                $page = $_GET['page'];
+            }
+
+            require_once($_SERVER['DOCUMENT_ROOT'].'/template/' . $page . '_tpl.php'); //Get Php files from /template
+
+        ?>
+
+    </body>
+
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/template/footer_tpl.php'); //Calling Footer ?>
+        
+</html>
